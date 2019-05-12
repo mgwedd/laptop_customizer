4cb80e95038814a6fd9f1f624c1ef950cd37e5f1
mgwedd <michael.wedd@gmail.com>
2019-05-11 23:35:53 -0400

    before componentizing the css

---
+++ src/CartSummary/CartSummary.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function CartSummary(props) {
+    const { selectedFeatures } = props;
+    const selectedFeatureCategories = Object.keys( selectedFeatures );
+    return (
+        selectedFeatureCategories
+            .map((category, index) => {
+                return (
+                    <div className="summary__option" key={ index }>
+                        <div className="summary__option__label">{ category }</div>
+                        <div className="summary__option__value">{ selectedFeatures[ category ].name }</div>
+                        <div className="summary__option__cost">
+                        { new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD'})
+                            .format(selectedFeatures[ category ].cost) }
+                        </div>
+                    </div>)
+                })
+    )
+}
--- src/ShoppingCart/ShoppingCart.js
+++ src/ShoppingCart/ShoppingCart.js
@@ -1,25 +1,16 @@
 import React from 'react';
+import CartSummary from '../CartSummary/CartSummary'
 
 export default function ShoppingCart( props ) {
     const { selectedFeatures } = props;
-    const cartSummary = Object.keys( selectedFeatures )
-                        .map((category, index) => {
-                        return (
-                            <div className="summary__option" key={ index }>
-                                <div className="summary__option__label">{ category }</div>
-                                <div className="summary__option__value">{ selectedFeatures[ category ].name }</div>
-                                <div className="summary__option__cost">
-                                { new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD'})
-                                    .format(selectedFeatures[ category ].cost) }
-                                </div>
-                            </div>)
-                        }); 
+ 
     const total = Object.keys( selectedFeatures )
         .reduce((acc, curr) => acc + selectedFeatures[ curr ].cost, 0);  
     return (
         <section className="main__summary">
             <h3>NEW GREENLEAF 2018</h3>
-            { cartSummary }
+            <CartSummary 
+                selectedFeatures={ selectedFeatures }/>
             <div className="summary__total">
                 <div className="summary__total__label">Your Price: </div>
                 <div className="summary__total__value">
